c3db42ca4d6892d654b48664b03dfe390327dcc8
Developer <{ID}+{username}@users.noreply.github.com>
2024-06-10 13:06:39 +0530

    btnClr

--- src/Card.js
+++ src/Card.js
@@ -49,7 +49,7 @@ function Card() {
                             <a onClick={() => {
                                 deleteMovie(movie.id)
                                 console.log('Deleted')
-                            }} className="cursor-pointer inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            }} className="cursor-pointer inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 ">
                                 Delete
 
                             </a>
